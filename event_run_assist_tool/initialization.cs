c03465eb96be2a454acdc6bafcbdf5eb6bf27e53
takusumi0617 <takusumi0617@gmail.com>
2024-07-27 09:19:28 +0900

    レジストリにパスを保存する機能

---
+++ event_run_assist_tool/initialization.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace event_run_assist_tool
+{
+    internal class initialization
+    {
+        public void initialize()
+        {
+
+        }
+    }
+}
