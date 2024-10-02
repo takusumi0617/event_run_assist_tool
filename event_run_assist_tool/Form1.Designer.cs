f2e9fe4225e5811cfb8b054b9e23ba3e02e5122b
takusumi0617 <takusumi0617@gmail.com>
2024-10-02 09:34:53 +0900

    Update Form1.Designer.cs

--- event_run_assist_tool/Form1.Designer.cs
+++ event_run_assist_tool/Form1.Designer.cs
@@ -42,18 +42,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Noto Sans JP", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(188, 24);
+            this.label1.Size = new System.Drawing.Size(231, 35);
             this.label1.TabIndex = 0;
             this.label1.Text = "イベント支援ツール";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 64);
+            this.button1.Location = new System.Drawing.Point(13, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 41);
+            this.button1.Size = new System.Drawing.Size(187, 58);
             this.button1.TabIndex = 1;
             this.button1.Text = "①イベラン計画を作成";
             this.button1.UseVisualStyleBackColor = true;
@@ -61,9 +63,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(213, 64);
+            this.button2.Location = new System.Drawing.Point(248, 59);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(160, 41);
+            this.button2.Size = new System.Drawing.Size(187, 58);
             this.button2.TabIndex = 2;
             this.button2.Text = "②イベラン計画を実行";
             this.button2.UseVisualStyleBackColor = true;
@@ -71,9 +74,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(414, 64);
+            this.button3.Location = new System.Drawing.Point(483, 59);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(160, 41);
+            this.button3.Size = new System.Drawing.Size(187, 58);
             this.button3.TabIndex = 3;
             this.button3.Text = "③イベラン計画を振り返る";
             this.button3.UseVisualStyleBackColor = true;
@@ -81,9 +85,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(414, 12);
+            this.button4.Location = new System.Drawing.Point(483, 17);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(87, 24);
+            this.button4.Size = new System.Drawing.Size(102, 34);
             this.button4.TabIndex = 4;
             this.button4.Text = "設定";
             this.button4.UseVisualStyleBackColor = true;
@@ -92,27 +97,30 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(178, 78);
+            this.label2.Location = new System.Drawing.Point(208, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.Size = new System.Drawing.Size(32, 17);
             this.label2.TabIndex = 5;
             this.label2.Text = "＞＞";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(379, 78);
+            this.label3.Location = new System.Drawing.Point(442, 78);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.Size = new System.Drawing.Size(32, 17);
             this.label3.TabIndex = 6;
             this.label3.Text = "＞＞";
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(530, 18);
+            this.linkLabel1.Location = new System.Drawing.Point(618, 26);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(44, 12);
+            this.linkLabel1.Size = new System.Drawing.Size(51, 17);
             this.linkLabel1.TabIndex = 7;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "License";
@@ -120,9 +128,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(611, 159);
+            this.ClientSize = new System.Drawing.Size(697, 136);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -131,7 +139,9 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Noto Sans JP", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "イベント支援ツール";
